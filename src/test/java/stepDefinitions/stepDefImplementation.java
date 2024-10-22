6bd78d8e5bdc8c87acad2da4a78ef6a2690f1438
varun <jm.varun222@gmail.com>
2024-10-22 19:53:14 +0530

    develop thingy

--- src/test/java/stepDefinitions/stepDefImplementation.java
+++ src/test/java/stepDefinitions/stepDefImplementation.java
@@ -24,6 +24,7 @@ public class stepDefImplementation {
         driver.get("https://rahulshettyacademy.com/client");
         landingPage = new LandingPage(driver);
         //no need of this println command bro.
+        System.out.println("what the bro");
 
 
     }
@@ -31,6 +32,7 @@ public class stepDefImplementation {
     @When("Logged in with username {} and password {}")
     public void loggedInWithUsernameAndPassword(String arg0, String arg1) {
         landingPage.loginApplication(arg0,arg1);
+        System.out.println("what the bro");
 
     }
 
@@ -39,6 +41,8 @@ public class stepDefImplementation {
     public void isToBeDisplayed(String arg0) {
         Assert.assertEquals(arg0,landingPage.getErrorMessage());
         driver.close();
+        System.out.println("what the bro");
+        System.out.println("what the bro");
 
     }
 }
