3ce660ce682e029de3f56382b523413fc64866c2
varun <jm.varun222@gmail.com>
2024-10-22 20:01:10 +0530

    hope this works

--- src/test/java/stepDefinitions/stepDefImplementation.java
+++ src/test/java/stepDefinitions/stepDefImplementation.java
@@ -23,8 +23,7 @@ public class stepDefImplementation {
         driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/client");
         landingPage = new LandingPage(driver);
-        //no need of this println command bro.
-        System.out.println("what the bro");
+
 
 
     }
@@ -32,7 +31,7 @@ public class stepDefImplementation {
     @When("Logged in with username {} and password {}")
     public void loggedInWithUsernameAndPassword(String arg0, String arg1) {
         landingPage.loginApplication(arg0,arg1);
-        System.out.println("what the bro");
+
 
     }
 
@@ -41,8 +40,7 @@ public class stepDefImplementation {
     public void isToBeDisplayed(String arg0) {
         Assert.assertEquals(arg0,landingPage.getErrorMessage());
         driver.close();
-        System.out.println("what the bro");
-        System.out.println("what the bro");
+
 
     }
 }
